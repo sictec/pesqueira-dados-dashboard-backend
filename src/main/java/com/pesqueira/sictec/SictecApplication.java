85ae67449e4210c303677ba56b32485bc0a124d3
Jamerson Carlos <64469941+JamersonCarlos@users.noreply.github.com>
2025-04-29 12:09:53 -0300

    inicializando backend

---
+++ src/main/java/com/pesqueira/sictec/SictecApplication.java
@@ -0,0 +1,13 @@
+package com.pesqueira.sictec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SictecApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SictecApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pesqueira/sictec/SictecApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pesqueira.sictec;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SictecApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
